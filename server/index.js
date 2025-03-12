239a386cacfc2ea3b551cb0f79bfffbba7e086d3
Arafin Mridul <mridularafin@gmail.com>
2025-03-12 16:39:31 +0600

    feat: add auth controller for handling requests and data from signup and login routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const bodyParser = require('body-parser');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
 
 dotenv.config();
 connectDB();
@@ -11,10 +11,14 @@ const app = express();
 
 app.use(bodyParser.json()); // To parse incoming JSON requests
 
-app.use('/api/auth', authRoutes);
+app.get("/", (req, res) => {
+    res.json({ message: "hi this is working" });
+});
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
