ac2cd2ef90b0d5e349f4cc63e30c17a9468ea9d3
Arafin Mridul <mridularafin@gmail.com>
2025-03-16 01:10:21 +0600

    feat: add event routes to manage and join event

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const eventRoutes = require("./routes/eventRoutes");
 const bodyParser = require("body-parser");
 
 dotenv.config();
@@ -19,6 +20,8 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 
 app.use("/api/users", userRoutes);
+
+app.use("/api/events", eventRoutes);
 
 const PORT = process.env.PORT || 5000;
 
