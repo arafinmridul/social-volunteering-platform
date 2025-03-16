4c0ed473af4bd611ae7947f018a71d7c058f8f0f
Arafin Mridul <mridularafin@gmail.com>
2025-03-17 00:36:45 +0600

    feat: add comment controller to read and add comments in a post

---
+++ server/controllers/commentController.js
@@ -0,0 +1,34 @@
+const Comment = require('../models/Comment');
+const Post = require('../models/Post');
+
+// Add a comment to a post
+const addComment = async (req, res) => {
+    try {
+        const { text } = req.body;
+        const post = await Post.findById(req.params.postId);
+
+        if (!post) return res.status(404).json({ message: 'Post not found' });
+
+        const newComment = new Comment({ postId: post._id, userId: req.user.id, text });
+        await newComment.save();
+
+        post.comments.push(newComment._id);
+        await post.save();
+
+        res.status(201).json(newComment);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
+// Get all comments for a post
+const getCommentsByPost = async (req, res) => {
+    try {
+        const comments = await Comment.find({ postId: req.params.postId }).populate('userId', 'name');
+        res.status(200).json(comments);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
+module.exports = { addComment, getCommentsByPost };
